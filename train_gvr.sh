dcf20d520daab9a677ea3d3c127c2ae4c7ad631c
ZixunH <zixun@berkeley.edu>
2025-04-19 16:58:54 -0700

    fix args

--- train_gvr.sh
+++ train_gvr.sh
@@ -1,10 +1,9 @@
 set -e
-SCENE_IDS="0a5c013435 1f7cbbdde1 1d003b07bd 0a7cc12c0e 2a1a3afad9"
+SCENE_IDS="2a1a3afad9 0a5c013435 1f7cbbdde1 1d003b07bd 4ef75031e3"
 DATA_ROOT="/media/scannetpp/demo/"
 
-STEP=2e-3
-FOVMOD_TRAIN=1.3
-FOVMOD_EVAL=2.0
+STEP=0.002
+FOVMOD_TRAIN=1.0 #1.3
 
 ITERS_NUM=30000
 
@@ -15,10 +14,8 @@ for SCENE_ID in $SCENE_IDS; do
     OUTPUT_DIR="./output/scannetpp/$SCENE_ID"
 
     FOVMAP_DIR_TRAIN="undistorted_fovmaps_fov_${FOVMOD_TRAIN}_step_${STEP}/"
-    FOVMAP_DIR_EVAL="undistorted_fovmaps_fov_${FOVMOD_EVAL}_step_${STEP}/"
 
     TRAIN_MASK_FN="fov_${FOVMOD_TRAIN}_step_${STEP}_mask.png"
-    TEST_MASK_FN="fov_${FOVMOD_EVAL}_step_${STEP}_mask.png"
 
     # Train: Generate fov map
     python prepare_fov.py --path "$DATASET_DIR" --dst "$FOVMAP_DIR_TRAIN" --step "$STEP" --fov_mod "$FOVMOD_TRAIN" --mask_dst "$TRAIN_MASK_FN"
@@ -26,8 +23,8 @@ for SCENE_ID in $SCENE_IDS; do
     # Train: Run training script
     python train.py -s "$DATASET_DIR" -m "$OUTPUT_DIR" \
         --iterations "$ITERS_NUM" \
-        --checkpoint_iterations 200 300 500 700 1000 2000 3000 4000 7000 8000 9000 10000 12000 15000 17000 20000 22000 25000 27000 30000 \
-        --save_iterations 200 300 500 700 1000 2000 3000 4000 7000 8000 9000 10000 12000 15000 17000 20000 22000 25000 27000 30000 \
+        --checkpoint_iterations 3000 7000 15000 30000 \
+        --save_iterations 3000 7000 15000 30000 \
         --test_iterations 200 300 500 700 1000 2000 3000 4000 7000 8000 9000 10000 12000 15000 17000 20000 22000 25000 27000 30000 \
         --resolution 1 \
         --eval \
